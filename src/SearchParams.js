5352e30a49594e4d9c94dfcfe34f060d90c63f3b
priya-co <loginpriyakumari@gmail.com>
2020-07-13 18:46:53 +0530

    UseEffect basics

---
+++ src/SearchParams.js
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
+
+const SearchParams = () => {
+  const [location, setLocation] = useState("Seattle, WA");
+  const [animal, setAnimal] = useState("dog");
+  const [breed, setBreed] = useState("");
+  const [breeds, setBreeds] = useState([]);
+
+  useEffect(() => {
+    setBreeds([]);
+    setBreed("");
+    pet.breeds(animal).then(({ breeds }) => {
+      const breedStrings = breeds.map(({ name }) => name);
+      setBreeds(breedStrings);
+    }, console.error);
+  }, [animal]);
+
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          location
+          <input
+            id="location"
+            placeholder="Location"
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+          />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => setAnimal(e.target.value)}
+            onBlur={(e) => setAnimal(e.target.value)}
+          >
+            <option>All</option>
+            {ANIMALS.map((animal) => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            disabled={!breeds.length}
+            id="breed"
+            value={breed}
+            onChange={(e) => setBreed(e.target.value)}
+            onBlur={(e) => setBreed(e.target.value)}
+          >
+            <option>All</option>
+            {breeds.map((breedString) => (
+              <option key={breedString} value={breedString}>
+                {breedString}
+              </option>
+            ))}
+          </select>
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
---
+++ src/useDropdown.js
@@ -0,0 +1,5 @@
+import React, { useState } from "react";
+
+const useDropdown = (label, defaultStatus, options) => {};
+
+export default useDropdown;
