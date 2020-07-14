0482342d878605f92329df7041f66b740880c0ae
priya-co <loginpriyakumari@gmail.com>
2020-07-14 14:14:10 +0530

    typo

--- src/Pet.js
+++ src/Pet.js
@@ -11,11 +11,13 @@ const Pet = (props) => {
   return (
     <a href={`/details/${id}`} className="pet">
       <div className="image-container">
-        <img src={hero} alt={name} />{" "}
-      </div>{" "}
+        <img src={hero} alt={name} />
+      </div>
       <div className="info">
         <h1> {name} </h1> <h2> {`${animal} — ${breed} — ${location}`} </h2>{" "}
-      </div>{" "}
+      </div>
     </a>
   );
 };
+
+export default Pet;
