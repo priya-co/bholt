0332bfdfb660057d535838914dd0161020f329e3
priya-co <loginpriyakumari@gmail.com>
2020-07-16 13:11:50 +0530

    Context with hooks and class componenet

--- src/Pet.js
+++ src/Pet.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "@reach/router";
 
 const Pet = (props) => {
   const { name, animal, breed, media, location, id } = props;
@@ -9,14 +10,14 @@ const Pet = (props) => {
   }
 
   return (
-    <a href={`/details/${id}`} className="pet">
+    <Link to={`/details/${id}`} className="pet">
       <div className="image-container">
         <img src={hero} alt={name} />
       </div>
       <div className="info">
         <h1> {name} </h1> <h2> {`${animal} — ${breed} — ${location}`} </h2>{" "}
       </div>
-    </a>
+    </Link>
   );
 };
 
